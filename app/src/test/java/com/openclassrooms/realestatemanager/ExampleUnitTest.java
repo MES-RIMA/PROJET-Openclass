9b2d9fab5f08a0a2cea55fc58329f2c7d55c999b
messabehi rima <91118960+MES-RIMA@users.noreply.github.com>
2023-12-17 22:37:33 +0100

    utils convertdollartoeuro and inverse

--- app/src/test/java/com/openclassrooms/realestatemanager/ExampleUnitTest.java
+++ app/src/test/java/com/openclassrooms/realestatemanager/ExampleUnitTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,5 +16,27 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void convertDollarToEuro() {
+        // 100$ =>93€
+        final int dollars = 100;
+        final int euros = Utils.convertDollarToEuro(dollars);
+        assertEquals(euros, 93);
+    }
+
+    @Test
+    public void convertEuroToDollar() {
+        // 108€ => 100$
+        final int euros = 100;
+        final int dollars = Utils.convertEuroToDollar(euros);
+        assertEquals(dollars, 108);
+    }
+
+    @Test
+    public void getTodayDateTest() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String actualDate = Utils.getTodayDate();
+        assertEquals(dateFormat.format(new Date()), actualDate);
     }
 }
